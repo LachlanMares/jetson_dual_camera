fe22f8dc2ee324eb74633ef69430f43020191e2b
Lachlan <lachlan.mares@adelaide.edu.au>
2023-02-21 09:08:54 +1030

    First Code Commit

---
+++ src/jetson_dual_camera/src/csi_camera.cpp
@@ -0,0 +1,128 @@
+/*
+Author:
+    Lachlan Mares, lachlan.mares@adelaide.edu.au
+
+License:
+    GPL-3.0
+
+Description:
+
+*/
+
+#include "ros/ros.h"
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+#include <sstream>
+#include <new>
+#include <string>
+#include <sstream>
+
+
+class RPi2Camera
+{
+    private:
+        // Node handles
+        ros::NodeHandle _nh;
+        ros::NodeHandle _nh_priv;
+
+        image_transport::ImageTransport _it;
+        
+        // Variables
+        bool _running;
+        int _device_id;
+        int _camera_mode;
+        int _flip_mode;
+        int _frame_rate;
+        int _image_width;
+        int _image_height;
+
+        std::string _image_topic;
+        std::string _gstreamer_str;
+
+        // Publishers
+        image_transport::Publisher _image_publisher;
+
+        // CV2
+        cv::VideoCapture _video;
+        cv::Mat _frame;
+
+        // Timers
+        ros::Timer _timer;
+
+    public:
+    
+        RPi2Camera(): _it(_nh), _nh(), _nh_priv("~") {
+
+            // Private Parameters
+            _nh_priv.param<int>("device_id", _device_id, 0);
+            _nh_priv.param<int>("camera_mode", _camera_mode, 3);
+            _nh_priv.param<int>("flip_mode", _flip_mode, 2);
+            _nh_priv.param<int>("frame_rate", _frame_rate, 10);
+            _nh_priv.param<int>("image_width", _image_width, 1280);
+            _nh_priv.param<int>("image_height", _image_height, 720);
+
+            std::ostringstream _image_topic_default;
+            _image_topic_default << "/camera_" << _device_id;
+            _nh_priv.param<std::string>("image_topic", _image_topic, _image_topic_default.str());
+
+            // Variables
+            _running = false;
+
+            int camera_capture_width[5] = {3280, 3280, 1920, 1280, 1280};
+            int camera_capture_height[5] = {2464, 1848, 1080, 720, 720};
+            int camera_capture_fps[5] = {21, 30, 30, 60, 90};
+
+            std::stringstream ss;
+            ss << "nvarguscamerasrc sensor-id=" << _device_id << " sensor-mode=" << _camera_mode << " ! video/x-raw(memory:NVMM), width=" << camera_capture_width[_camera_mode] << ", height=" << camera_capture_height[_camera_mode] << ", format=NV12, framerate=" << camera_capture_fps[_camera_mode] << "/1 ! nvvidconv flip-method=" << _flip_mode << " ! video/x-raw, width=" << _image_width << ", height=" << _image_height << ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+ 
+            _video.open(ss.str());
+
+            if (!_video.isOpened())
+            {
+                ROS_ERROR("Unable to get video from the camera!");
+            }
+
+            _image_publisher = _it.advertise(_image_topic, 1);
+
+            // Timers
+            _timer = _nh.createTimer(ros::Duration(1.0/((double)_frame_rate)), &RPi2Camera::timerCallback, this);
+        }
+
+        ~RPi2Camera() {}
+
+        void start_capture() {
+            _running = true;
+        }
+
+        void stop_capture() {
+            _running = false;
+        }
+
+        void timerCallback(const ros::TimerEvent &) { 
+            if (_running && ros::ok()) {
+                if (_video.read(_frame)) { 
+                    cv_bridge::CvImage camera_msg; 
+                    camera_msg.encoding = sensor_msgs::image_encodings::BGR8;
+                    camera_msg.image = _frame; 
+                    _image_publisher.publish(camera_msg.toImageMsg());
+                } 
+            }           
+        }
+};
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "camera_node");
+  
+  RPi2Camera rp_cam;
+
+  rp_cam.start_capture();
+
+  ros::spin();
+
+  return 0;
+}
