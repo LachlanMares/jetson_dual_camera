41210eca1c331cf6795ec8c67ec177310b32f16e
Lachlan <lachlan.mares@adelaide.edu.au>
2023-02-21 15:32:13 +1030

    Tasty mods

---
+++ src/jetson_dual_camera/include/camera_utils.hpp
@@ -0,0 +1,78 @@
+/*
+Author:
+    Lachlan Mares, lachlan.mares@adelaide.edu.au
+
+License:
+    GPL-3.0
+
+Description:
+
+*/
+
+#include <ros/ros.h>
+#include <sstream>
+
+std::string get_gstreamer_str(int camera_id, int camera_mode, int flip_method, int width, int height) {
+
+    std::stringstream gs_str;
+
+    switch(camera_mode) {     
+        case 0: 
+            gs_str << "nvarguscamerasrc sensor-id=" << camera_id << " sensor-mode=0 ! video/x-raw(memory:NVMM), width=3280, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=" << flip_method << " ! video/x-raw, width=" << width << ", height=" << height << ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+            break;
+
+        case 1: 
+            gs_str << "nvarguscamerasrc sensor-id=" << camera_id << " sensor-mode=1 ! video/x-raw(memory:NVMM), width=3280, height=1848, format=NV12, framerate=30/1 ! nvvidconv flip-method=" << flip_method << " ! video/x-raw, width=" << width << ", height=" << height << ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+            break;
+
+        case 2: 
+            gs_str << "nvarguscamerasrc sensor-id=" << camera_id << " sensor-mode=2 ! video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=30/1 ! nvvidconv flip-method=" << flip_method << " ! video/x-raw, width=" << width << ", height=" << height << ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+            break;
+
+        case 3: 
+            gs_str << "nvarguscamerasrc sensor-id=" << camera_id << " sensor-mode=3 ! video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=60/1 ! nvvidconv flip-method=" << flip_method << " ! video/x-raw, width=" << width << ", height=" << height << ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+            break;
+
+        case 4: 
+            gs_str << "nvarguscamerasrc sensor-id=" << camera_id << " sensor-mode=4 ! video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=90/1 ! nvvidconv flip-method=" << flip_method << " ! video/x-raw, width=" << width << ", height=" << height << ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+            break;
+        
+        default:
+            gs_str << "nvarguscamerasrc sensor-id=" << camera_id << " sensor-mode=0 ! video/x-raw(memory:NVMM), width=3280, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method=" << flip_method << " ! video/x-raw, width=" << width << ", height=" << height << ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+            break;
+    }
+
+    return gs_str.str();
+}
+
+double get_timer_period(int fps, int camera_mode) {
+    double timer_period = 1.0;
+
+    switch(camera_mode) {  
+        case 0: 
+            timer_period = 1.0 / (std::max(std::min(fps, 21), 1));
+            break;
+        
+        case 1:
+            timer_period = 1.0 / (std::max(std::min(fps, 30), 1));
+            break;
+        
+        case 2:
+            timer_period = 1.0 / (std::max(std::min(fps, 30), 1));
+            break;
+        
+        case 3:
+            timer_period = 1.0 / (std::max(std::min(fps, 60), 1));
+            break;
+        
+        case 4:
+            timer_period = 1.0 / (std::max(std::min(fps, 120), 1));
+            break;
+
+        default:
+            timer_period = 1.0 / (std::max(std::min(fps, 21), 1));
+            break;
+    }
+
+    return timer_period;
+}
--- src/jetson_dual_camera/src/csi_camera.cpp
+++ src/jetson_dual_camera/src/csi_camera.cpp
@@ -10,6 +10,7 @@ Description:
 */
 
 #include "ros/ros.h"
+#include <camera_utils.hpp>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
@@ -19,7 +20,6 @@ Description:
 #include <sstream>
 #include <new>
 #include <string>
-#include <sstream>
 
 
 class RPi2Camera
@@ -35,13 +35,13 @@ class RPi2Camera
         bool _running;
         int _device_id;
         int _camera_mode;
-        int _flip_mode;
+        int _flip_method;
         int _frame_rate;
         int _image_width;
         int _image_height;
 
         std::string _image_topic;
-        std::string _gstreamer_str;
+        // std::string _gstreamer_str;
 
         // Publishers
         image_transport::Publisher _image_publisher;
@@ -59,11 +59,11 @@ class RPi2Camera
 
             // Private Parameters
             _nh_priv.param<int>("device_id", _device_id, 0);
-            _nh_priv.param<int>("camera_mode", _camera_mode, 3);
-            _nh_priv.param<int>("flip_mode", _flip_mode, 2);
-            _nh_priv.param<int>("frame_rate", _frame_rate, 10);
-            _nh_priv.param<int>("image_width", _image_width, 1280);
-            _nh_priv.param<int>("image_height", _image_height, 720);
+            _nh_priv.param<int>("camera_mode", _camera_mode, 0);
+            _nh_priv.param<int>("flip_method", _flip_method, 0);
+            _nh_priv.param<int>("frame_rate", _frame_rate, 21);
+            _nh_priv.param<int>("image_width", _image_width, 3280);
+            _nh_priv.param<int>("image_height", _image_height, 2464);
 
             std::ostringstream _image_topic_default;
             _image_topic_default << "/camera_" << _device_id;
@@ -72,24 +72,21 @@ class RPi2Camera
             // Variables
             _running = false;
 
-            int camera_capture_width[5] = {3280, 3280, 1920, 1280, 1280};
-            int camera_capture_height[5] = {2464, 1848, 1080, 720, 720};
-            int camera_capture_fps[5] = {21, 30, 30, 60, 90};
+            //std::string g_string = get_gstreamer_str(_device_id, _camera_mode, _flip_method, _image_width, _image_height);
 
-            std::stringstream ss;
-            ss << "nvarguscamerasrc sensor-id=" << _device_id << " sensor-mode=" << _camera_mode << " ! video/x-raw(memory:NVMM), width=" << camera_capture_width[_camera_mode] << ", height=" << camera_capture_height[_camera_mode] << ", format=NV12, framerate=" << camera_capture_fps[_camera_mode] << "/1 ! nvvidconv flip-method=" << _flip_mode << " ! video/x-raw, width=" << _image_width << ", height=" << _image_height << ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
+            //std::stringstream ss;
+            //ss << "nvarguscamerasrc sensor-id=" << _device_id << " sensor-mode=" << _camera_mode << " ! video/x-raw(memory:NVMM), width=" << camera_capture_width[_camera_mode] << ", height=" << camera_capture_height[_camera_mode] << ", format=NV12, framerate=" << camera_capture_fps[_camera_mode] << "/1 ! nvvidconv flip-method=" << _flip_method << " ! video/x-raw, width=" << _image_width << ", height=" << _image_height << ", format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink";
  
-            _video.open(ss.str());
+            _video.open(get_gstreamer_str(_device_id, _camera_mode, _flip_method, _image_width, _image_height));
 
-            if (!_video.isOpened())
-            {
+            if (!_video.isOpened()) {
                 ROS_ERROR("Unable to get video from the camera!");
             }
 
             _image_publisher = _it.advertise(_image_topic, 1);
 
             // Timers
-            _timer = _nh.createTimer(ros::Duration(1.0/((double)_frame_rate)), &RPi2Camera::timerCallback, this);
+            _timer = _nh.createTimer(ros::Duration(get_timer_period(_frame_rate, _camera_mode)), &RPi2Camera::timerCallback, this);
         }
 
         ~RPi2Camera() {}
@@ -106,6 +103,7 @@ class RPi2Camera
             if (_running && ros::ok()) {
                 if (_video.read(_frame)) { 
                     cv_bridge::CvImage camera_msg; 
+                    camera_msg.header.stamp = ros::Time::now();
                     camera_msg.encoding = sensor_msgs::image_encodings::BGR8;
                     camera_msg.image = _frame; 
                     _image_publisher.publish(camera_msg.toImageMsg());
