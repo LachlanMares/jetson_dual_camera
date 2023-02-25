ee385dc7cd3edf8ae072dbff00ca45e7fbc16414
Lachlan <lachlan.mares@gmail.com>
2023-02-25 21:50:04 +1030

    Modified to create .mp4's for both camera feeds from one launch file

--- src/jetson_dual_camera/scripts/video_creator.py
+++ src/jetson_dual_camera/scripts/video_creator.py
@@ -3,26 +3,47 @@
 import os
 import cv2
 import rospy
+import subprocess
 from pathlib import Path
+from threading import Thread
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+
+
+def bag_loop():
+    global bag_filename, bag_directory
+    with subprocess.Popen(['rosbag', 'play', str(bag_filename)], cwd=bag_directory) as bag_player:
+        stdoutdata, stderrdata = bag_player.communicate()
+        if bag_player.returncode == 0:
+            waiting = False
 
 
 if __name__ == "__main__":
     """ Don't ask me why video encoding has to happen in the main thread, it just does """
     rospy.init_node("video_creator")
+    global bag_filename, bag_directory
+
+    if rospy.get_param(param_name="~start_rosbag"):
+        bag_directory = Path(rospy.get_param(param_name="~rosbag_directory"))
+        bag_filename = bag_directory / rospy.get_param(param_name="~rosbag_filename")
+
+        bag_loop_thread = Thread(target=bag_loop, daemon=True)
 
     jetson_dual_camera_dir = Path(os.getenv("HOME")) / 'jetson_dual_camera' / 'src' / 'jetson_dual_camera' / 'videos'
-    video_title = jetson_dual_camera_dir / rospy.get_param(param_name="video/title")
-    frame_rate = rospy.get_param(param_name="video/frame_rate")
-    width = rospy.get_param(param_name="video/width")
-    height = rospy.get_param(param_name="video/height")
-    image_topic = rospy.get_param(param_name="image_topic")
+
+    video_title = jetson_dual_camera_dir / rospy.get_param(param_name="~video/title")
+    frame_rate = rospy.get_param(param_name="~video/frame_rate")
+    width = rospy.get_param(param_name="~video/width")
+    height = rospy.get_param(param_name="~video/height")
+    image_topic = rospy.get_param(param_name="~image_topic")
 
     bag_timeout = False
     first_frame = True
     bridge = CvBridge()
     video_writer = cv2.VideoWriter(str(video_title), cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), frame_rate, (width, height))
+
+    if rospy.get_param(param_name="~start_rosbag"):
+        bag_loop_thread.start()
 
     # Wait until the rosbag is started
     data = rospy.wait_for_message(image_topic, Image, timeout=30)
@@ -31,7 +52,7 @@ if __name__ == "__main__":
         if not bag_timeout:
             try:
                 if not first_frame:
-                    data = rospy.wait_for_message(image_topic, Image, timeout=1)
+                    data = rospy.wait_for_message(image_topic, Image, timeout=2)
                 else:
                     first_frame = False
 
