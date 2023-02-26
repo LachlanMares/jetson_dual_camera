031a39fa8f99687eccf526eb4a8b13d251422002
Lachlan <lachlan.mares@adelaide.edu.au>
2023-02-27 10:16:57 +1030

    start not launch you derpy prick

--- src/jetson_dual_camera/remote_start_scripts/remote_start_nano_cameras.sh
+++ src/jetson_dual_camera/remote_start_scripts/remote_start_nano_cameras.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 trap 'ssh 192.168.0.1 "pkill roslaunch"' INT TERM
-ssh jetson@192.168.0.1 "~/jetson_dual_camera/src/jetson_dual_camera/remote_launch_scripts/env_nano.sh"
+ssh jetson@192.168.0.1 "~/jetson_dual_camera/src/jetson_dual_camera/remote_start_scripts/env_nano.sh"
 wait
